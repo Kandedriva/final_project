1ea4b711f292b7b230a9409ee771ca2fcdf5ff61
drissakande <drissakande@gmail.com>
2023-09-07 12:48:37 -0400

    Add code in the CreateJob component to control the form

--- src/components/CreateJob.js
+++ src/components/CreateJob.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function CreateJob() {
+  const [newJob, setnewJob] = useState({
+    company: "",
+    title: "",
+    link: "",
+    experience: "",
+    salary: "",
+    location: "",
+    description: ""
+  });
+  function handleChange(event){
+    const name = event.target.name;
+    let value = event.target.value;
+    setnewJob({
+      ...newJob,
+      [name]: value,
+    })
+  }
+  function submitJob(event){
+    event.preventDefault();
+
+  }
     return (
       <form className="createJob" action="index.html" method="post">
         <h3>Open a position and receive the candidats you need</h3>
@@ -11,49 +32,49 @@ function CreateJob() {
         <div className="jobCreation">
           {/* <label htmlFor="company">Company's name:</label> */}
           
-          <input className="createJobInput" type="text" name="company" value="" />
+          <input className="createJobInput" type="text" name="company" value={newJob.company}  onChange={handleChange}/>
         </div>
         <label htmlFor="title">Job Title:</label>
 
         <div className="jobCreation">
           {/* <label htmlFor="title">Job Title:</label> */}
-          <input className="createJobInput" type="text" name="title" value="" />
+          <input className="createJobInput" type="text" name="title" value={newJob.title}  onChange={handleChange}/>
         </div>
         <label htmlFor="link">Company's website:</label>
 
   
         <div className="jobCreation">
           {/* <label htmlFor="link">Company's website:</label> */}
-          <input className="createJobInput" type="text" name="link" value="" />
+          <input className="createJobInput" type="text" name="link" value={newJob.link}  onChange={handleChange}/>
         </div>
         <label htmlFor="experience">Experiences required:</label>
 
   
         <div className="jobCreation">
           {/* <label htmlFor="experience">Experiences required:</label> */}
-          <input className="createJobInput" type="text" name="experience" value="" />
+          <input className="createJobInput" type="text" name="experience" value={newJob.experience} onChange={handleChange}/>
         </div>
         <label htmlFor="salary">Average salary/hour:</label>
 
   
         <div className="jobCreation">
           {/* <label htmlFor="salary">Average salary/hour:</label> */}
-          <input className="createJobInput" type="text" name="salary" value="" />
+          <input className="createJobInput" type="text" name="salary" value={newJob.salary}  onChange={handleChange}/>
         </div>
         <label htmlFor="location">Job's Location:</label>
 
         <div className="jobCreation">
           {/* <label htmlFor="location">Job's Location:</label> */}
-          <input className="createJobInput" type="text" name="location" value="" />
+          <input className="createJobInput" type="text" name="location" value={newJob.location}  onChange={handleChange}/>
         </div>
         <label htmlFor="description">Job description:</label>
 
          <div className="description">
           {/* <label htmlFor="description">Job description:</label> */}
-          <textarea name="description" rows="15" cols="80"></textarea>
+          <textarea name="description" rows="15" cols="80" onChange={handleChange} value={newJob.description}></textarea>
         </div>
   
-        <button className="signUpButton" type="button" name="button">
+        <button className="signUpButton" type="submit" name="button" onSubmit={submitJob}>
           Post the Job
         </button>
       </form>

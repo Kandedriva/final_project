42d2ad84ab9f4a8ef46b1ec7ad54faab601d802c
drissakande <drissakande@gmail.com>
2023-09-09 09:20:33 -0400

    Make get request to display the Available job list

--- src/components/ApplicationList.js
+++ src/components/ApplicationList.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JobApplicationFormat from "./JobApplicationFormat";
+import JobApplication from "./JobApplication";
 
 function ApplicationList(){
+    const [applications, setApplications] = useState([]);
 
+useEffect(() => {
+    fetch("https://phase-2-banckend.onrender.com/waitingList ")
+    .then(response=>response.json())
+    .then(data=>setApplications(data))
+    
+}, []);
+
+function position(newpost){
+    setApplications([...applications, newpost])
+}
+
+return(
+    <div>
+            {applications.map((application)=>
+            <JobApplicationFormat 
+            key={application.id} 
+            fName={application.applicationName} 
+            lName={application.lastName} 
+            email={application.email} 
+            history={application.history} 
+            education={application.education} 
+            availability={application.availability}
+
+            />)}
+            <JobApplication position={position}/>
+
+    </div>
+)
 }
 
 export default ApplicationList

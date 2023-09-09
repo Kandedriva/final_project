37bfab8d37255bf42132e6fd85891f491dc4a84e
drissakande <drissakande@gmail.com>
2023-09-09 09:24:05 -0400

    Make fetch Request display the list of position the user applied for

--- src/components/AvailableJobList.js
+++ src/components/AvailableJobList.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AvailableJobFormat from "./AvailableJobFormat";
 
 function AvailableJobList(){
-    return
+const [jobs, setJobs] = useState([]);
+
+useEffect(() => {
+    fetch("https://phase-2-banckend.onrender.com/availableJobs")
+    .then(response=>response.json())
+    .then(data=>setJobs(data))
+
+   
+}, []);
+
+    return(
+        <div>
+            {jobs.map((displayjob)=><AvailableJobFormat key={displayjob.id} title={displayjob.jobTitle} Company={displayjob.company} experiences={displayjob.experience} average={displayjob.salary} location={displayjob.location} description={displayjob.description} website={displayjob.website}/>)}
+        </div>
+    )
 }
 
 export default AvailableJobList;

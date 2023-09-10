5a6ee50eec41a8d8b477624690cf90615641b28a
drissakande <drissakande@gmail.com>
2023-09-10 10:14:44 -0400

    Change the code inside the Welcome component tu use Bootstrap

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { Form, Button } from 'react-bootstrap';
 
 
 
@@ -23,7 +24,33 @@ function createClick(){
       <div className="homePage">
             {/* <nav><button className="goback" onClick={backToLogIn}> Go back</button></nav> */}
 
-      <h1>Welcome to Dream Job</h1>
+            <Form.Group controlId="formAddress">
+<h1>Welcome to Dream Job</h1>
+<span>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+</span>
+<Button
+type="button" 
+name="button"
+ onClick={logInClick}
+ variant="outline-primary" 
+ className="sign">
+ Log In
+ </Button>{' '}
+<Button
+type="button" 
+name="button" 
+onClick={logInClick}
+ variant="outline-success"
+  className="sign">
+  Register
+  </Button>{' '}
+          
+        </Form.Group>
+
+      {/* <h1>Welcome to Dream Job</h1>
         <div >
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
@@ -35,7 +62,7 @@ function createClick(){
           <button className="sign" type="button" name="button" onClick={createClick}>
           Sign Up  
           </button>
-        </div>
+        </div> */}
         </div>
      
     );
